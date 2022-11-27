1e9d3a3176e7d060155453b899566e0cd96607f1
rmassey95 <rmassey.95@gmail.com>
2022-11-27 12:33:26 -0500

    Add logic for checking optimal moves

---
+++ script.js
@@ -0,0 +1,147 @@
+const NodeFactory = (move, edge = [], prevNode = null, distance = null) => {
+  this.move = move;
+  this.edge = edge;
+  this.prevNode = prevNode;
+  this.distance = distance;
+
+  return { move, edge, prevNode, distance };
+};
+
+const Tree = () => {
+  const possibleMoves = [
+    [-2, -1],
+    [-1, -2],
+    [-2, 1],
+    [-1, 2],
+    [1, 2],
+    [2, 1],
+    [2, -1],
+    [1, -2],
+  ];
+
+  let graph = [];
+
+  for (let i = 0; i < 8; i++) {
+    for (let x = 0; x < 8; x++) {
+      let node = NodeFactory([i, x]);
+
+      let moves = [];
+
+      for (let e = 0; e < possibleMoves.length; e++) {
+        let arr = [];
+        arr.push(node.move[0] + possibleMoves[e][0]);
+        arr.push(node.move[1] + possibleMoves[e][1]);
+
+        if (arr[0] >= 0 && arr[0] <= 7 && arr[1] >= 0 && arr[1] <= 7) {
+          moves.push(arr);
+        }
+      }
+
+      node.edge = moves;
+
+      graph.push(node);
+    }
+  }
+
+  const find = (pos) => {
+    for (let i = 0; i < graph.length; i++) {
+      if (pos[0] == graph[i].move[0] && pos[1] == graph[i].move[1]) {
+        return graph[i];
+      }
+    }
+  };
+
+  const knightMoves = (pos, end, queue = [], first = true) => {
+    let node = find(pos);
+
+    if (first) {
+      node.distance = 0;
+      first = false;
+    }
+
+    if (node.move[0] === end[0] && node.move[1] === end[1]) {
+      return node;
+    }
+
+    let prevNode = node;
+    node.edge.forEach((edge) => {
+      if (find(edge).distance === null) {
+        queue.push(edge);
+        find(edge).prevNode = prevNode;
+        find(edge).distance = prevNode.distance + 1;
+      }
+    });
+
+    return knightMoves(queue.shift(), end, queue, first);
+  };
+
+  return { knightMoves };
+};
+
+const bst = Tree();
+
+let endNode = bst.knightMoves([3, 3], [1, 2]);
+
+const printMoves = (node) => {
+  if (node === null) {
+    return;
+  }
+
+  printMoves(node.prevNode);
+
+  console.log(node.move);
+};
+
+console.log(`You made it in ${endNode.distance} moves! Here's your path:`);
+printMoves(endNode);
+
+// bst.knightMoves([3, 3], [4, 3]);
+
+// const NodeFactory = (move, children) => {
+//   this.move = move;
+//   this.children = children;
+// };
+
+// const Tree = (startPos, endPos) => {
+//   const possibleMoves = [
+//     [-2, -1],
+//     [-1, -2],
+//     [-2, 1],
+//     [-1, 2],
+//     [1, 2],
+//     [2, 1],
+//     [2, -1],
+//     [1, -2],
+//   ];
+
+//   const buildTree = (startPos, endPos) => {
+//     let moves = [];
+
+//     for (let i = 0; i < possibleMoves.length; i++) {
+//       let arr = [];
+//       arr.push(startPos[0] + possibleMoves[i][0]);
+//       arr.push(startPos[1] + possibleMoves[i][1]);
+
+//       if ((arr[0] >= 0 && arr[0] <= 7) || (arr[1] >= 0 && arr[1] <= 7)) {
+//         moves.push(arr);
+//       }
+//       if (arr[0] === endPos[0] && arr[1] === endPos[1]) {
+//         console.log("MATCH");
+//         break;
+//       }
+//     }
+
+//     let node = NodeFactory(startPos, moves);
+//     return node;
+//   };
+
+//   let root = buildTree(startPos, endPos);
+
+//   return { root };
+// };
+
+// function knightMoves(startPos, endPos) {}
+
+// const bst = Tree([3, 3], [1, 4]);
+
+// console.log(bst.root);
